162a191cab8ad7e67a1ea1b5b34e60b71c10da67
sakibHasan7439 <sakibhasanb120@gmail.com>
2025-05-01 17:24:49 +0600

    marqee swiper are added

--- src/app/Components/Landing/hero/Hero.jsx
+++ src/app/Components/Landing/hero/Hero.jsx
@@ -27,7 +27,7 @@ const Hero = () => {
                     <div className={`bg-[#F6F6F6] md:pb-20 lg:pb-28 xl:pb-32 py-2 md:pt-4 lg:pt-4 md:px-0 ${poppins.className}`}>
                         <div className="max-w-7xl mx-auto px-2 flex justify-between flex-col-reverse md:flex-row">
                             <div>
-                            <h1 className='md:text-2xl text-2xl pt-4 md:pt-4 xl:pt-8 lg:text-[35px] font-medium xl:text-[55px] pb-4 lg:pb-8 text-primary-color xl:leading-16'>Boost Your Health Naturally with Our Supplement</h1>
+                            <h1 className='md:text-2xl text-xl pt-4 md:pt-4 xl:pt-8 lg:text-[35px] font-medium xl:text-[55px] pb-4 lg:pb-8 text-primary-color xl:leading-16'>Boost Your Health Naturally with Our Supplement</h1>
                             <p className='text-secondary-color lg:text-[18px] pb-4 md:pb-8 lg:pb-10 max-w-[760px]'>
                                 Give your body the essential nutrients it deserves with our premium,all-natural supplements. Whether you're looking to boost your vitality, enhance performance, or maintain a healthy lifestyle, we've got you covered.
                             </p>
@@ -38,7 +38,7 @@ const Hero = () => {
                                 alt='hero image'
                                 width={590}
                                 height={460}
-                                className='md:w-[360px] md:h-[300px] lg:h-[400px] lg:w-[500px]'/>
+                                className='md:w-[360px] w-[220px] h-[180px] md:h-[300px] lg:h-[400px] lg:w-[500px] mx-auto'/>
                         </div>
                     </div>
                 </SwiperSlide>
@@ -46,7 +46,7 @@ const Hero = () => {
                 <div className={`bg-[#F6F6F6] md:pb-20 lg:pb-24 xl:pb-32 py-2 md:pt-4 lg:pt-8 md:px-0 ${poppins.className}`}>
                         <div className="max-w-7xl mx-auto px-2 flex justify-between flex-col-reverse md:flex-row">
                             <div>
-                            <h1 className='md:text-2xl text-2xl pt-4 md:pt-4 xl:pt-8 lg:text-[35px] font-medium xl:text-[55px] pb-4 lg:pb-8 text-primary-color xl:leading-16'>Boost Your Health Naturally with Our Supplement</h1>
+                            <h1 className='md:text-2xl text-xl pt-4 md:pt-4 xl:pt-8 lg:text-[35px] font-medium xl:text-[55px] pb-4 lg:pb-8 text-primary-color xl:leading-16'>Boost Your Health Naturally with Our Supplement</h1>
                             <p className='text-secondary-color lg:text-[18px] pb-4 md:pb-8 lg:pb-10 max-w-[760px]'>
                                 Give your body the essential nutrients it deserves with our premium,all-natural supplements. Whether you're looking to boost your vitality, enhance performance, or maintain a healthy lifestyle, we've got you covered.
                             </p>
@@ -57,7 +57,7 @@ const Hero = () => {
                                 alt='hero image'
                                 width={590}
                                 height={460}
-                                className='md:w-[360px] md:h-[300px] lg:h-[400px] lg:w-[500px]'/>
+                                className='md:w-[360px] mx-auto w-[220px] h-[180px] md:h-[300px] lg:h-[400px] lg:w-[500px]'/>
                         </div>
                     </div>
                 </SwiperSlide>
@@ -65,7 +65,7 @@ const Hero = () => {
                 <div className={`bg-[#F6F6F6] md:pb-20 py-2 xl:pb-32 md:py-8 lg:pb-24 md:pt-4 lg:pt-8 md:px-0 ${poppins.className}`}>
                         <div className="max-w-7xl mx-auto px-2 flex justify-between flex-col-reverse md:flex-row">
                             <div>
-                            <h1 className='md:text-2xl text-2xl pt-4 md:pt-4 xl:pt-8 lg:text-[35px] font-medium xl:text-[55px] pb-4 lg:pb-8 text-primary-color xl:leading-16'>Boost Your Health Naturally with Our Supplement</h1>
+                            <h1 className='md:text-2xl text-xl pt-4 md:pt-4 xl:pt-8 lg:text-[35px] font-medium xl:text-[55px] pb-4 lg:pb-8 text-primary-color xl:leading-16'>Boost Your Health Naturally with Our Supplement</h1>
                             <p className='text-secondary-color lg:text-[18px] pb-4 md:pb-8 lg:pb-10 max-w-[760px]'>
                                 Give your body the essential nutrients it deserves with our premium,all-natural supplements. Whether you're looking to boost your vitality, enhance performance, or maintain a healthy lifestyle, we've got you covered.
                             </p>
@@ -76,7 +76,7 @@ const Hero = () => {
                                 alt='hero image'
                                 width={590}
                                 height={460}
-                                className='md:w-[360px] md:h-[300px] lg:h-[400px] lg:w-[500px]'
+                                className='md:w-[360px] mx-auto w-[220px] h-[180px] md:h-[300px] lg:h-[400px] lg:w-[500px]'
                                 />
                         </div>
                     </div>
--- src/app/introduce-truemed/IntroduceTruemed.jsx
+++ src/app/introduce-truemed/IntroduceTruemed.jsx
@@ -1,8 +1,25 @@
+"use client"
+import { useState } from "react";
 import SectionSubtitle from "../Components/Landing/section-subtitle/SectionSubtitle";
 import SectionTitle from "../Components/Landing/section-title/SectionTitle";
 import Button from "../Shared/Button/Button";
+import { Modal } from "antd";
+import Image from "next/image";
 
 const IntroduceTruemed = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const showModal = () => {
+      setIsModalOpen(true);
+    };
+  
+    const handleOk = () => {
+      setIsModalOpen(false);
+    };
+  
+    const handleCancel = () => {
+      setIsModalOpen(false);
+    };
     return (
         <div className="bg-[#F8F8F8] px-2 pt-10 md:pt-12 pb-17 mb-6 lg:mb-14 xl:mb-30">
             <div className="max-w-7xl mx-auto">
@@ -11,7 +28,36 @@ const IntroduceTruemed = () => {
                     subtitle={"Boost your health naturally every day with our premium supplement, designed to support energy, immunity, and overall wellness using only high-quality ingredients."}
                 />
 
-                <iframe src="https://www.youtube.com/embed/r8YUIZGQ6ZU?si=E1AApxdYhqx_duwr" className="h-[200px] md:h-[320px] xl:h-[540px] xl:rounded-4xl rounded-2xl w-full mx-auto lg:w-full mb-14" title="YouTube video player" frameBorder="0"></iframe>
+                  <div type="primary" className="relative mb-12 cursor-pointer" onClick={showModal}>
+                        <Image 
+                            src={"/images/supplyment.png"}
+                            width={700}
+                            height={540}
+                            className="w-full object-cover rounded-2xl h-[250px] xl:h-[540px]"
+                            alt="supplyment image"/>
+                        
+                        <Image 
+                            src={"/images/image (7).png"}
+                            width={700}
+                            height={540}
+                            className="w-full object-cover rounded-2xl h-[250px] xl:h-[540px] absolute top-0"
+                            alt="supplyment image"/>
+                        
+                        <Image
+                            src={"/images/play.png"}
+                            width={50}
+                            height={50}
+                            className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+                            alt="play button"
+                        />
+                    </div>
+                    <Modal 
+                        open={isModalOpen} 
+                        onCancel={handleCancel} 
+                        width="100%" >
+                    <iframe src="https://www.youtube.com/embed/r8YUIZGQ6ZU?si=E1AApxdYhqx_duwr" className="h-[200px] md:h-[320px] xl:h-[540px] xl:rounded-4xl rounded-2xl w-full mx-auto lg:w-full mb-14" title="YouTube video player" frameBorder="0"></iframe>
+                    </Modal>
+                
                 <div className="flex justify-center">
                     <Button text={"Learn More About Truemed"}/>
                 </div>

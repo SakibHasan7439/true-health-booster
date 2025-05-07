7ea72cd5013d8c92b1a63d67d006a001ff56aad1
sakibHasan7439 <sakibhasanb120@gmail.com>
2025-05-07 21:35:49 +0600

    some issue fixed

--- src/app/Components/contact-page/contact-info/ContactCard.jsx
+++ src/app/Components/contact-page/contact-info/ContactCard.jsx
@@ -5,7 +5,7 @@ const ContactCard = ({ info }) => {
   return (
     <>
       <div
-  className="relative bg-[url(/images/carveCard.png)] rounded-[20px] px-5 py-6 xl:px-9 xl:py-11 w-full overflow-hidden"
+  className="relative bg-[url(/images/carveCard.png)] bg-cover bg-center rounded-[20px] px-5 py-6 xl:px-9 xl:py-11 w-full overflow-hidden"
 >
   <div className="flex items-center gap-3 mb-4">
     <div className="bg-cyan-500 p-2 text-white rounded-full w-10 lg:w-15 lg:h-15 xl:h-20 xl:w-20 mb-3 lg:mb-4 h-10 flex items-center justify-center text-xl">
@@ -21,7 +21,7 @@ const ContactCard = ({ info }) => {
 
   <p className="text-sm lg:text-lg border-t pt-4 lg:pt-6 xl:pt-3">{info?.contact}</p>
 
-    <div style={{border: "10px solid white"}} className="absolute w-8 h-8 lg:w-11 lg:h-11 xl:w-15 xl:h-15 rounded-full -bottom-2 hover:bg-[#32BADE] hover:text-white flex items-center cursor-pointer justify-center -right-2">
+    <div style={{border: "10px solid white"}} className="absolute w-8 h-8 lg:w-11 lg:h-11 xl:w-17 xl:h-17 rounded-full -bottom-2 hover:bg-[#32BADE] hover:text-white flex items-center cursor-pointer justify-center -right-2">
       <ArrowUpRight/>
     </div>
       </div>

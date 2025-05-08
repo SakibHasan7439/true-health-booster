c49165407948b263dff5af7be3cef9c8b93fdf89
sakibHasan7439 <sakibhasanb120@gmail.com>
2025-05-08 15:09:46 +0600

    some modification done

--- src/app/Components/product-details/from-the-brand/FromBrandCard.jsx
+++ src/app/Components/product-details/from-the-brand/FromBrandCard.jsx
@@ -1,9 +1,17 @@
 const FromBrandCard = ({info}) => {
     return (
-        <div className=" bg-cover bg-center flex flex-col justify-end h-[140px] md:h-[260px] xl:h-[400px] text-center bg-no-repeat px-2 py-2 text-white" style={{ backgroundImage:  `url(${info?.img})` }}>
-            <h3 className="lg:text-[15px] xl:text-xl text-[12px] text-sm font-medium border-b-2 mb-2"><i>{info?.title}</i></h3>
-            <p className="text-[8px] md:text-[12px] xl:text-sm">{info?.text}</p>
-        </div>
+        <div
+            className="relative bg-cover bg-center flex flex-col justify-end h-[140px] md:h-[260px] xl:h-[400px] text-center bg-no-repeat px-2 py-2 text-white"
+            style={{ backgroundImage: `url(${info?.img})` }}
+            >
+            <div className="absolute inset-0 bg-[#0000001b] z-10"></div>
+            <div className="relative z-20">
+                <h3 className="lg:text-[15px] xl:text-xl text-[12px] text-sm font-medium border-b-2 mb-2">
+                <i>{info?.title}</i>
+                </h3>
+                <p className="text-[8px] md:text-[12px] xl:text-sm">{info?.text}</p>
+            </div>
+            </div>
     );
 };
 
